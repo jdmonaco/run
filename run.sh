3da61c3c7ec4cadf8209ae94e9528b6b3a83cb49
Joe Monaco <self@joemona.co>
2016-07-01 10:35:52 -0400

    Add init metacommand to create new runfile

--- run.sh
+++ run.sh
@@ -1,16 +1,55 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 #
 # Script runner
 #
 
-RUNFILE=runfile
-SH=`which sh`
+RUNFILE="./runfile"
+BASH=`which bash`
+VIM=vim
 
-if [ -f "./$RUNFILE" ]
+if [ -f "$RUNFILE" ]
 then
-    $SH "./$RUNFILE" ${@}
-else
-    echo "No runfile found."
+    $BASH "$RUNFILE" ${@}
+    exit 0
+fi
+
+if [[ $# == 1 ]] && [[ "$1" == "init" ]]
+then
+(
+cat <<RUN
+#!/usr/bin/env sh
+set -ue
+
+#
+# Runfile script
+#
+# Add commands as test cases below.
+#
+
+USAGE="Usage: `basename \${0}` command"
+
+if [[ \$# != 1 ]]
+then
+    echo \$USAGE
     exit 1
 fi
+
+case "\$1" in
+
+test) echo "Testing runfile command." ;;
+
+*)
+    echo \$USAGE
+    exit 1
+    ;;
+
+esac
+RUN
+) > "$RUNFILE" &&
+    $VIM "$RUNFILE"
+exit 0
+fi
+
+echo "No runfile found."
+exit 1
